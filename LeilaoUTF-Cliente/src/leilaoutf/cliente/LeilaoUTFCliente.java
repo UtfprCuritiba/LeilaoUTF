284965a89452a6fe136f6add6e8f1b9a163be16d
a1305093 <a1305093@localhost>
2015-04-06 20:54:56 -0300

    Teste commit.

--- LeilaoUTF-Cliente/src/leilaoutf/cliente/LeilaoUTFCliente.java
+++ LeilaoUTF-Cliente/src/leilaoutf/cliente/LeilaoUTFCliente.java
@@ -16,6 +16,7 @@ public class LeilaoUTFCliente {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Teste Commit");
         
     }
     
