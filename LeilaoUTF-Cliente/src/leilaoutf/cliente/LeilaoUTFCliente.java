f8d2cfc35420f48d5e10f435c5140dc11373a12e
Mayko <Mayko@192.168.108.144>
2015-04-06 21:08:46 -0300

    blalalba

--- LeilaoUTF-Cliente/src/leilaoutf/cliente/LeilaoUTFCliente.java
+++ LeilaoUTF-Cliente/src/leilaoutf/cliente/LeilaoUTFCliente.java
@@ -16,7 +16,8 @@ public class LeilaoUTFCliente {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Teste Commit");
+        System.out.println("Olá mundo");
+        System.out.println("Olá mundo");
         
     }
     
